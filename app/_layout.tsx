3d8440848a20f12e02aea6b5d0c9772ca26df104
Alberto Luti <betomdq.93@gmail.com>
2025-04-18 10:57:24 -0300

    Feat: upgrade expo and remove useColorScheme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,8 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
+import { useColorScheme } from 'react-native';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
