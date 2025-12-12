bdb1b20c9e67b8c0a6a09ef5971c896eecffd3df
Alberto Luti <betomdq.93@gmail.com>
2025-12-12 20:19:01 -0300

    Feat: add generic useFetcherApp hook

---
+++ hooks/useListFetcherApp.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export function useListFetcherApp<T>(
+  fetchFn: () => Promise<T[]>,
+  deps: any[] = []
+) {
+  const [data, setData] = useState<T[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    fetchWrapper();
+  }, deps);
+
+  function fetchWrapper() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      fetchFn()
+        .then((data) => {
+          setData(data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          setError(error);
+        });
+    } catch (error) {
+      setIsLoading(false);
+      setError(error as Error);
+    }
+  }
+
+  return { data, isLoading, error, refetch: fetchWrapper };
+}
