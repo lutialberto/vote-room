c1e7d65303bb41cd86bf38ab3f11d0ed4091e7bb
Alberto Luti <betomdq.93@gmail.com>
2026-02-27 14:48:29 -0300

    Feat: format countDownApp value to display

--- components/CountDownApp.tsx
+++ components/CountDownApp.tsx
@@ -17,10 +17,37 @@ export default function CountDownApp({
   if (!isRunning && !isFinished) {
     start(seconds);
   }
+
+  const totalSeconds = currentValue ?? 0;
+
+  const oneMinute = 60;
+  const oneHour = 3600;
+  const oneDay = 86400;
+
+  const displayDays = Math.floor(totalSeconds / oneDay);
+  const displayHours =
+    totalSeconds >= oneHour ? Math.floor((totalSeconds % oneDay) / oneHour) : 0;
+  const displayMinutes =
+    totalSeconds >= oneMinute
+      ? Math.floor((totalSeconds % oneHour) / oneMinute)
+      : 0;
+  const displaySeconds = totalSeconds % oneMinute;
+
+  let formattedTime: string;
+  if (displayDays > 0) {
+    formattedTime = `${displayDays}d ${displayHours}:${displayMinutes}:${displaySeconds}`;
+  } else if (displayHours > 0) {
+    formattedTime = `${displayHours}:${displayMinutes}:${displaySeconds}`;
+  } else if (displayMinutes > 0) {
+    formattedTime = `${displayMinutes}:${displaySeconds}`;
+  } else {
+    formattedTime = `${displaySeconds}`;
+  }
+
   return (
     <>
       {isRunning && !isFinished && (
-        <ThemedText>Tiempo restante: {currentValue} segundos</ThemedText>
+        <ThemedText>Tiempo restante: {formattedTime}</ThemedText>
       )}
     </>
   );
